db54b03cc03577546086edfc25538b93d65a81c7
DulanMahesh <dulansurawera@gmail.com>
2025-06-04 11:09:10 +0200

    day 5 -Hangman Game

--- Python exercises/Day 05/HangmanGame.py
+++ Python exercises/Day 05/HangmanGame.py
@@ -24,28 +24,33 @@ def asking_to_guess():
     revealed_letters=['-'for n in secret_word]#list comprehension to showing "-" for letters in the secret_word.once correct letter is guess.this variable is updated automatically
     guessed_letters=[] #previously correctly guessed letters
     while tries>0 and '-'in revealed_letters:
-        result = []
+
         guessed_letter= input('Guess the letters in the secret word:').lower()
         if guessed_letter in guessed_letters: #check to see if the newly guessed letter is already guessed previously
             print("you aleady guessed that letter.Try a new one!!")
             continue
         else:
-            guessed_letters.append(guessed_letter) #if the new letter is not previously guesed,appened the
+            guessed_letters.append(guessed_letter) #if the new letter is not previously guesed,appened the guessed_letter list
 
-
-
-        for w in secret_word:
-            if w==guessed_letter:
-                result.append(w)
-
-            else:
-                result.append('-')
+        for index,letter in enumerate(secret_word) :# use enumerate to know the index of the letter position
+            if letter==guessed_letter:
+                revealed_letters[index]=guessed_letter # if the guessed letter is equal to the letter in the secret_word , that guessed letter will be stored in the correct position of the revelated_letters list
 
         if guessed_letter not in (secret_word):#check to see if guess letter is not in the secret word.if it not in the secret word, reduce one "try"
             tries =tries-1
             print(f'you entered wrong letter.you have only {tries} lifes left!!')
 
-        print (''.join(result))#join the strings together without spaces between letters
+
+        print (''.join(revealed_letters))#join the strings together without spaces between letters.because now the list is having relvelaed letters like this['f','-','i','-','-'].now we want to show the player what he has guss like this --->> f-i--
+
+        if tries==0 and '-' in revealed_letters:
+            print(f'Sorry!! You lost the game.because you ran out of lives.The secret word is: {secret_word} ')
+
+        if '-'not in (revealed_letters):
+            print(f'congratulation you won.The secret word is : {''.join(revealed_letters)}')
+
+
+
 asking_to_guess()
 
 
